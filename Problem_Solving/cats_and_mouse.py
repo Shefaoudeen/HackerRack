beed5ff1a41a9da6527bded27a955a41e0eebe33
Shefaoudeen <shefaoudeen123@gmail.com>
2023-10-10 22:28:19 +0530

    problem solved

--- Problem_Solving/cats_and_mouse.py
+++ Problem_Solving/cats_and_mouse.py
@@ -4,10 +4,9 @@ n = int(input())
 
 for i in range(0, n):
     parameter = list(map(int, input().split()))
-    distance1 = math.trunc(parameter[2]-parameter[0])
-    distance2 = math.trunc(parameter[2]-parameter[1])
+    distance1 = abs(parameter[2]-parameter[0])
+    distance2 = abs(parameter[2]-parameter[1])
 
-    print(distance1, distance2)
     if (distance1 < distance2):
         print("Cat A")
     elif (distance2 < distance1):
